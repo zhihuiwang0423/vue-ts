3d9805be918cf5bcff9c8505737c3a38a96430ba
zhihuiwang0423 <zhihuiwang0423@gmail.com>
2019-02-28 16:03:38 +0800

    初级搭建

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <input v-model="msg">
+    <p>msg:{{msg}}</p>
+    <p>computed msg ：{{computedMsg}}</p>
+    <button @click="greet">Func</button>
+  </div>
+</template>
+<script lang='ts'>
+import Vue from 'vue'
+// 为了定义带有装饰风格的组件 
+import Component from 'vue-class-component'
+@Component  //装饰器
+export default class HelloWorld extends Vue{
+  msg = 124
+  // 计算属性
+  get computedMsg (){
+    return 'computed'+this.msg
+  }
+  // 方法
+  Func(){
+    alert('greeting'+ this.msg)
+  }
+  mounted(){
+    this.Func()
+  }
+}
+// export default {
+//   data() {
+//     return {
+//       msg: "zhihui"
+//     };
+//   },
+//   computed: {
+//     computedMsg(newvalue, oldvalue) {
+//       return "computed" + this.msg;
+//     }
+//   },
+//   methods: {
+//     Func() {
+//       alert("greeting" + this.msg);
+//     }
+//   },
+// mounted () {
+//   this.Func()
+// }
+// };
+</script>
